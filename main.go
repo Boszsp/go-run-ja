733acc506118bc379fdbda97d7c6b66742fdb62b
The BoszSpace <95701554+Boszsp@users.noreply.github.com>
2024-11-04 14:46:38 +0700

    Update main.go

--- main.go
+++ main.go
@@ -5,23 +5,90 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"os"
 	"os/exec"
+	"path"
+	"strconv"
 	"strings"
 	"time"
 )
 
+var inputSet []string = make([]string, 2)
 
-var inputSet []string = []string{"5555"}
+func t() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(1)*time.Second)
 
-func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	tempPath, err := os.MkdirTemp("./", "runJava")
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(0)
+	} else {
+		os.Chmod(tempPath, 0o777)
+	}
+	os.Chdir(tempPath)
+	defer os.RemoveAll(path.Join("..",tempPath))
+	code := `
+
+	import java.util.Scanner;
+
+	/*
+	 * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+	 * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+	 */
+	/**
+	 *
+	 * @author xxx
+	 */
+	public class CodeJv {
+	
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			int name = sc.nextInt();
+			int b = sc.nextInt();
+			CodeJv2.hello();
+			System.out.println( b + (name) );
+		}
+	
+	}
+	
+	`
+
+	os.WriteFile("CodeJv.java", []byte(code), 0o777)
+	code = `
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author xxx
+ */
+public class CodeJv2 {
+    static public void  hello(){
+        System.out.println("Hello! From CodeJv2");
+    }
+}
+
+	`
+	os.WriteFile("CodeJv2.java", []byte(code), 0o777)
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(0)
+	}
 
 	defer cancel()
 	defer func() {
-		exec.Command("./clearComplier.sh").Run()
+		exec.Command("./clearComplier.sh", ".").Run()
 	}()
+
 	//Complier state
-	exec.Command("javac", "CodeJv.java").Run()
+	time.Sleep(time.Duration(10) * time.Millisecond)
+	comp := exec.Command("javac", "CodeJv.java")
+	comp.Run()
+
+
 	cmd := exec.CommandContext(ctx, "java", "CodeJv.java")
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
@@ -29,7 +96,7 @@ func main() {
 	}
 	go func() {
 		defer stdin.Close()
-		for _,input := range(inputSet){
+		for _, input := range inputSet {
 			io.WriteString(stdin, string(input))
 			io.WriteString(stdin, "\n")
 		}
@@ -40,9 +107,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	go func ()  {
+	go func() {
 		defer stderr.Close()
-        wtf, _ = io.ReadAll(stderr)
+		wtf, _ = io.ReadAll(stderr)
 	}()
 
 	stdout, err := cmd.StdoutPipe()
@@ -50,14 +117,30 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	go func ()  {
+	go func() {
 		defer stdout.Close()
-        out, _ = io.ReadAll(stdout)
+		out, _ = io.ReadAll(stdout)
 	}()
 
 	cmd.Run()
 
+	fmt.Print(strings.Trim(string(out), ""), map[bool]string{false: "", true: "Error = "}[string(wtf) != ""], string(wtf))
+	if(ctx.Err() != nil){
+		fmt.Println(ctx.Err())
+	}
+}
 
-	fmt.Print(strings.Trim(string(out), ""),string(wtf))
+func main() {
 
+	for i := 1; i <= 1; i++ {
+		inputSet[0] = strconv.Itoa(i*50)
+		inputSet[1] = strconv.Itoa(i + 100)
+
+		if i == 3 {
+			inputSet[1] = "xxxx"
+
+		}
+		fmt.Println("Input: ", inputSet)
+		t()
+	}
 }
